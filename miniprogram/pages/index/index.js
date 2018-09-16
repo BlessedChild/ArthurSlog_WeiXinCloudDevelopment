b75add2c7c07aeda10b347d9a210c0ff0888c02f
BlessedChild <2250970846@qq.com>
2018-09-17 03:15:05 +0800

    update

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,20 +1,22 @@
-//index.js
+// index.js
 const app = getApp()
 
 Page({
   data: {
     avatarUrl: './user-unlogin.png',
     userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: '',
+    logged: false, // 用来判断用户是否已经授权过了
     fileID: '',
     cloudPath: '',
-    imagePath: './user-unlogin.png',
-    openid:'',
-    sumResult:'',
+    imagePath: '',
+    openid: '',
+    sumResult: '',
+    page_iq: '',
+    user_nickName: '',
+    user_dbFlag: false,
   },
 
+  // 小程序页面打开时，默认执行的函数
   onLoad: function() {
     if (!wx.cloud) {
       wx.redirectTo({
@@ -23,6 +25,7 @@ Page({
       return
     }
 
+    const this_ = this
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -32,7 +35,32 @@ Page({
             success: res => {
               this.setData({
                 avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
+                userInfo: res.userInfo,
+                user_nickName: res.userInfo.nickName,
+                logged: true,
+                user_dbFlag: true
+              })
+              app.globalData.logged = true
+              app.globalData.user_dbFlag = true
+              app.globalData.openid = res.userInfo.openid
+
+              const db = wx.cloud.database()
+              const _ = db.command
+              // 读取用户数据，并渲染在页面上
+              db.collection('Users').where({
+                _openid: _.eq(app.globalData.openid)// 填入当前用户 openid
+              }).get().then(res => {
+                console.log(res.data)
+                if (res.data != null) {
+                  // 如果用户存在数据
+                  // 拉取用户的IQ数据
+                  this_.setData({
+                    page_iq: res.data[0].IQ,
+                    user_dbFlag: true
+                  })
+                } else {
+                  console.log("拉取云端数据出错")
+                }
               })
             }
           })
@@ -40,86 +68,121 @@ Page({
       }
     })
   },
+  // 查询数据库，获取当前用户数据
+  // 修改数据库信息，更新当前用户数据
+  arthurSlog_readingBook: function() {
+    const this_ = this
+    // 验证是否已经授权
+    if (app.globalData.logged) {
+      // 验证云端是否存在用户数据
+      const db = wx.cloud.database()
+      const _ = db.command
+      // 读取用户数据，并渲染在页面上
+      db.collection('Users').where({
+        _openid: _.eq(app.globalData.openid) // 填入当前用户 openid
+      }).get().then(res => {
+        console.log("查询数据库")
+        if (!res.data) {
+          console.log("云端数据库没有返回数据")
+          console.log("用户在数据库没有数据，现在为用户创建数据库")
+          // 创建用户数据
+          db.collection('Users').add({
+              // data 字段表示需新增的 JSON 数据
+              data: {
+                userName: app.globalData.user_nickName,
+                userDate: db.serverDate(),
+                IQ: '10'
+              }
+            })
+            .then(res => {
+              console.log("用户数据创建成功")
+              console.log(res)
+            }).catch(console.error)
 
+          app.globalData.user_dbFlag = true
+        } else if (res.data) {
+          console.log("云端数据库成功返回数据")
+          console.log('更新数据库')
+          console.log('当前的全局_id值是： ' + app.globalData._id)
+          app.globalData.IQ = res.data[0].IQ
+          // 如果该用户有数据
+          // 页面上IQ的值加1
+          // 数据库IQ的值加1
+          db.collection('Users').doc(app.globalData.id).update({
+            data: {
+              IQ: _.inc(1)
+            }
+          }).then(res => {
+            console.log('成功更新数据库')
+            console.log(res)
+            this_.setData({
+              page_iq: ++app.globalData.IQ
+            })
+          })
+        }
+      })
+    } else if (!app.globalData.logged) {
+      // 弹出提示框，显示“请点击头像授权”
+      wx.showToast({
+        icon: 'none',
+        title: '请先点击左上角允许授权',
+      })
+    }
+  },
+  // 点击弹出“微信授权” 窗口
+  // 用户点击“允许”，允许开发者获得用户的公开信息（昵称、头像等）
   onGetUserInfo: function(e) {
+    const this_ = this
     if (!this.logged && e.detail.userInfo) {
       this.setData({
         logged: true,
         avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
+        userInfo: e.detail.userInfo,
+        user_nickName: e.detail.userInfo.nickName
       })
+      app.globalData.openid = e.detail.userInfo.openId
+      // 需要先获得用户信息才能查询数据库
+      // 验证云端是否存在用户数据
+      const db = wx.cloud.database()
+      // 读取用户数据，并渲染在页面上
+      db.collection('Users').where({
+        _openid: app.globalData.openid // 填入当前用户 openid
+      }).get().then(res => {
+        if (res.data[0] != null) {
+          // 如果用户存在数据
+          // 拉取用户的IQ数据
+          this_.setData({
+            page_iq: res.data[0].IQ,
+            user_dbFlag: true
+          })
+        } else {
+          // 创建用户数据
+          db.collection('Users').add({
+              // data 字段表示需新增的 JSON 数据
+              data: {
+                userName: app.globalData.user_nickName,
+                userDate: db.serverDate(),
+                IQ: 10,
+                id: app.globalData.openid
+              }
+            })
+            .then(res => {
+                this_.setData({
+                  page_iq: '10',
+                  user_dbFlag: true
+                })
+              // 添加新表成功之后，打印数据库的新用户信息
+              console.log(res)
+              app.globalData._id = res._id
+            })
+
+          this_.setData({
+            user_dbFlag: true
+          })
+        }
+      })
+      app.globalData.logged = true
     }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
-  // 上传图片
-  doUpload: function() {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function(res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
   },
   // 添加前端代码，向后端服务发起 名为”arthurSlog_getInfo“方法的请求
   // 请求的结果会返回，并保存再 res对象中
@@ -134,6 +197,7 @@ Page({
         this_.setData({
           openid: res.result.openId
         })
+        app.globalData.openid = res.result.openId
       }
     })
   },
@@ -160,9 +224,8 @@ Page({
       })
       .catch(console.error)
   },
-
   // 添加前端代码,向云端上传图片
-  arthurSlog_uploadImg: function () {
+  arthurSlog_uploadImg: function() {
     // 选择图片
     const this_ = this
     wx.chooseImage({
